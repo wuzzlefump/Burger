8c7f72b1a82708d3253c8ba07eea90ee27048df6
wuzzlefump <paulelliottwork@gmail.com>
2020-09-17 16:35:06 -0600

    database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Krabby Patty');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name, eaten) VALUES ('veggie burger?', true);
+INSERT INTO burgers (name, eaten) VALUES ('Bison Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Hamburger"', true);
+INSERT INTO burgers (name) VALUES ('Chilli burger');
