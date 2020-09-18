dae27cc1c08669f64a87e9410cc8a5a554b79005
wuzzlefump <paulelliottwork@gmail.com>
2020-09-17 23:03:50 -0600

    Jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "macademiaNuts1",
   database: "da_burger_db"
 });
+}
+
 
 
 connection.connect(function(err) {
